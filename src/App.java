38a5dde40ee287514bf26900ac27ee135af0ee90
Elliana Rico <ellianarico24@Ellianas-MacBook-Pro.local>
2023-01-30 17:55:26 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("This is my Assignment 4");
+    }
+}
+
+
+
